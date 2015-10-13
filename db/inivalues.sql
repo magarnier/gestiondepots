4c666b0285de473c8358d03fa7953ab1fe636036
magarnier <garniermariea@gmail.com>
2015-10-13 20:27:50 +0200

    changement nom colonne

--- db/inivalues.sql
+++ db/inivalues.sql
@@ -9,7 +9,7 @@ INSERT INTO `equipe` (`idequipe`, `nomequipe`) VALUES
 (1, 'Team A'),
 (2, 'Team B');
 
-INSERT INTO `employe` (`idemploye`, `nomemploye`, `nombredepot`, `fkentreprise`, `fkequipe`) VALUES
+INSERT INTO `employe` (`idemploye`, `profilgithub`, `nombredepot`, `fkentreprise`, `fkequipe`) VALUES
 (1, 'nterray', 3, 1, 1),
 (2, 'yannis-rossetto', 14, 1, 1),
 (3, 'sandrae', 5, 1, 2);
